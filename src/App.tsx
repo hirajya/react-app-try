cc43a69a87374fcc6dd5fdd932313eaceec26151
hirajya <rodneyestrada2425@gmail.com>
2024-07-24 11:17:01 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+}
+
+export default App;
